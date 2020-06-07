881121fd92af362fa23cc8e61a7ad7acb7e0ab5d
takacpa <t.ura0315@gmail.com>
2020-06-07 12:32:04 +0900

    create devise_views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_070920) do
+ActiveRecord::Schema.define(version: 2020_06_07_032341) do
 
   create_table "debts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 2020_05_02_070920) do
     t.integer "repayment_cycle_id", null: false
     t.decimal "interrest_rate", precision: 10, null: false
     t.integer "company_id", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
