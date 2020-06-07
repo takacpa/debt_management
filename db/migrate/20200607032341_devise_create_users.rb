7fcc0701d3a11838344c694056f9a08aacff7c1b
takacpa <t.ura0315@gmail.com>
2020-06-07 12:25:12 +0900

    create model_user

--- db/migrate/20200607032341_devise_create_users.rb
+++ db/migrate/20200607032341_devise_create_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreate:s < ActiveRecord::Migration[5.2]
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table ::s do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -32,14 +32,13 @@ class DeviseCreate:s < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :install
 
       t.timestamps null: false
     end
 
-    add_index ::s, :email,                unique: true
-    add_index ::s, :reset_password_token, unique: true
-    # add_index ::s, :confirmation_token,   unique: true
-    # add_index ::s, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
